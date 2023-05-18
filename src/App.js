ef6836c83f12a410a29f457a7c937bf3fc07339a
Jared <jbitanga88@gmail.com>
2023-05-18 11:27:52 -0700

    part1 changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-const App = (props) => {
-  const {counter} = props
+import { useState} from 'react'
+import Display from './Display'
+import Button from './Button'
+import History from './History'
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+    setTotal(left + right)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+    setTotal(left + right)
+  }
+
   return (
-    <div>{counter}</div>
+    <div>
+    {left}
+    <Button handleClick={handleLeftClick} text='left' />
+    <Button handleClick={handleRightClick} text='right' />
+    {right}
+    <History allClicks={allClicks} />
+  </div>
   )
 }
-
 export default App
--- src/index.js
+++ src/index.js
@@ -3,16 +3,5 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-let counter = 1
 
-const refresh = () => {
-    ReactDOM.createRoot(document.getElementById('root')).render(<App counter={counter} />)
-}
-
-refresh()
-counter += 1
-refresh()
-counter += 1
-refresh()
-
-
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
